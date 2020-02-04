17aa28061b1bad6ca7f15d136791f83921b1d67c
Beck-Haywood <beck.haywood@students.makeschool.com>
2020-02-03 18:32:37 -0800

    homepage displays news lots of work still needs to be done

--- newsfeed/models.py
+++ newsfeed/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
+
+# class Article(models.Model):
+#     title = models.CharField(max_length=150, null=True)
+#     author = models.CharField(max_length=150, null=True)
+#     description = models.CharField(max_length=300, null=True)
+#     content = models.CharField(max_length=300, null=True)
+#     source = models.CharField(max_length=100, null=True)
+#     link = models.CharField(max_length=100, null=True)
+#     image = models.ImageField(upload_to='',
+#                               verbose_name='Image', null=True)
+#     slug = models.CharField(max_length=20, blank=True, editable=False, null=True)
+#     def __str__(self):
+#         return self.title
+#     def get_absolute_url(self):
+#         """ Returns a fully-qualified path for an article. """
+#         path_components = {'slug': self.slug}
+#         return reverse('', kwargs=path_components)
+
+#     def save(self, *args, **kwargs):
+#         """ Creates a URL safe slug automatically when a new an article is created. """
+#         if not self.pk:
+#             self.slug = slugify(self.title, allow_unicode=True)
+
+#         # Call save on the superclass.
+#         return super(Article, self).save(*args, **kwargs)
--- newsfeed/views.py
+++ newsfeed/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
 from newsapi.newsapi_client import NewsApiClient
+from newsapi import NewsApiClient 
 import requests
 import json
 import pandas
 
-# Create your views here.
+# Create your views here.  
+def index(request): 
+      
+    newsapi = NewsApiClient(api_key ='1757fcbd46b14cdea7eda377d1906a60') 
+    top = newsapi.get_top_headlines(sources ='techcrunch') 
+  
+    l = top['articles'] 
+    desc =[] 
+    news =[] 
+    img =[] 
+  
+    for i in range(len(l)): 
+        f = l[i] 
+        news.append(f['title']) 
+        desc.append(f['description']) 
+        img.append(f['urlToImage']) 
+    mylist = zip(news, desc, img) 
+  
+    return render(request, 'index.html', context ={"mylist":mylist}) 
 def newsfeed(request):
     template_name = 'newsfeed.html'
+    number_of_articles = 3
+
     headers = {'Authorization': '1757fcbd46b14cdea7eda377d1906a60'}
     top_headlines_url = 'https://newsapi.org/v2/top-headlines'
     everything_news_url = 'https://newsapi.org/v2/everything'
@@ -25,36 +46,54 @@ def newsfeed(request):
 
     print(response_data['status'])
     print(response_data['totalResults'])
-    article1 = response_data['articles']
-    #print(article1)
     print('-------------------------------')
-    print(article1[1])
-    print(article1[1]['source']['name'])
-    source = article1[1]['source']['name']
-    author = article1[1]['author']
-    title = article1[1]['title']
-    url = article1[1]['url']
-    content = article1[1]['content']
-    image = article1[1]['urlToImage']
-    description = article1[1]['description']
+
+
+    dic = {}
+
+    for i in range(number_of_articles):
+        print(i)
+
+        articles = response_data['articles']
+        source = articles[i]['source']['name']
+        author = articles[i]['author']
+        title = articles[i]['title']
+        url = articles[i]['url']
+        content = articles[i]['content']
+        image = articles[i]['urlToImage']
+        description = articles[i]['description']
+        #dic[i] = [source, author, title, url, content, image, description]
+        dic[str(i)] = {'source': source, 'author': author, 'title': title, 'url': url, 'content': content, 'image': image, 'description': description}
+    # print(dic)
+
+    #print(article1)
+    # print('-------------------------------')
+    # print(article1[1])
+    # print(article1[1]['source']['name'])
+    # source = article1[1]['source']['name']
+    # author = article1[1]['author']
+    # title = article1[1]['title']
+    # url = article1[1]['url']
+    # content = article1[1]['content']
+    # image = article1[1]['urlToImage']
+    # description = article1[1]['description']
 
 
 
 
-    pretty_json_output = json.dumps(response.json(), indent=4)
+    #pretty_json_output = json.dumps(response.json(), indent=4)
     #print(pretty_json_output)
-    response_json_string = json.dumps(response.json())
-    response_dict = json.loads(response_json_string)
+    #response_json_string = json.dumps(response.json())
+    #response_dict = json.loads(response_json_string)
 
 
-    articles_list = response_dict['articles']
+    #articles_list = response_dict['articles']
 
-    df = pandas.read_json(json.dumps(articles_list))
-    context = {'source': source, 'title': title, 'author': author, 'url': url, 'content': content, 'image': image, 'description': description}
+    #df = pandas.read_json(json.dumps(articles_list))
+    #context = {'source': source, 'title': title, 'author': author, 'url': url, 'content': content, 'image': image, 'description': description}
+    #context = {'dic': dic, 'number_of_articles': number_of_articles}
+    context = dic
+    print(context)
+   
 
-
-
-
- 
-
-    return render(request, template_name, context)
+    return render(request, template_name, {"context": context})
